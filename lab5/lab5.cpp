24122c399feeec421eac8e2fbcca519dc0a7d301
starsynapse <starsynapse@gmail.com>
2016-02-09 00:48:04 +0000

    just starting

---
+++ lab5/lab5.cpp
@@ -0,0 +1,19 @@
+/*
+ * Name        : lab_5.cpp
+ * Author      : Eduardo Zamora
+ * Description : Practicing Functions. Use this file to write your
+ *               Function Definitions (what goes below main()).
+ */
+
+#include "lab_5.h"
+
+// CODE HERE (FUNCTION DEFINITIONS)
+void Hello()
+{
+    cout << "Hello world!";
+}
+
+void PrintMessage(const string&)
+{
+    
+}

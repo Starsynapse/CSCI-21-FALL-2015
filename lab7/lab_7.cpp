abc1c66bb5976d0b9670bdefe36e3963d812f4e4
starsynapse <starsynapse@gmail.com>
2016-02-17 23:46:59 +0000

    Getting Started

---
+++ lab7/lab_7.cpp
@@ -0,0 +1,17 @@
+/*
+ * Name        : lab_7.cpp
+ * Author      : Eduardo Zamora
+ * Description : Working with File I/O
+ */
+#include "lab_7.h"
+
+// CODE HERE -- FUNCTION DEFINITION FOR ProcessFile()
+bool ProcessFile(string filename)
+{
+    
+}
+
+void ProcessArguments(int argc, char* argv[])
+{
+    
+}

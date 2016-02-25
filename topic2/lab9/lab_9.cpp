d9f61a0e2d3cbcac972387beb15a931cc0e44748
starsynapse <starsynapse@gmail.com>
2016-02-25 00:00:43 +0000

    lab9 finished

--- topic2/lab9/lab_9.cpp
+++ topic2/lab9/lab_9.cpp
@@ -6,7 +6,7 @@
 #include "lab_9.h"
 
 // CODE HERE -- CLASS FUNCTION DEFINITIONS
-void Spaceship::set_name(string name)
+void Spaceship::set_name(string name)//the following stuff just sets values
 {
   name_ = name;
 }
@@ -26,7 +26,7 @@ void Spaceship::set_crew_capacity(int crew_capacity)
   crew_capacity_ = crew_capacity;
 }
 
-string Spaceship::name() const
+string Spaceship::name() const//the following just get values
 {
   return name_;
 }
@@ -50,13 +50,23 @@ string Spaceship::ToString() const
 {
   stringstream ss;
   ss.str();
+  
+  ss.setf(std::ios::fixed);
+  ss.setf(std::ios::showpoint);
+  ss.precision(2);
+  //these lines set the precision of a number to a certain numberical place
+  
   string specifications;
+  //string for output
   
   ss << name_ << "\n";
   ss << "Top Speed:     Warp " << top_speed_ << "\n";
   ss << "Fuel Source:   " << fuel_source_ << "\n";
   ss << "Crew Capacity: " << crew_capacity_;
+  //conbines the values into a stringstream
   
   specifications = ss.str();
+  //string is set to equal the stringstream
+  
   return specifications;
 }

2e5cb6371770ee814a5c93103c55c5e3b166ed0a
starsynapse <starsynapse@gmail.com>
2016-02-27 00:29:42 +0000

    lab10 is done and commented

--- topic2/lab10/temperature.cpp
+++ topic2/lab10/temperature.cpp
@@ -8,54 +8,65 @@
 
 // CLASS FUNCTION DEFINITIONS GO HERE
 Temperature::Temperature()
+//if nothing is specified kelvin defaults to zero
 {
   kelvin_ = 0;
 }
 
 Temperature::Temperature(double kelvin)
+//if one parameter is specified temperature is set in kelvin
 {
   kelvin_ = kelvin;
 }
 
 Temperature::Temperature(double temp, char unit)
+//if two parameters are specified this function will convert the temperature
 {
   if (unit == 'C' || unit == 'c')
+  //converts from celsius to kelvin
   {
     kelvin_ = temp + 273.15;
   }
   
   else if (unit == 'F' || unit == 'f')
+  //converts from fahrenheit to kelvin
   {
     kelvin_ = (5.0 * (temp - 32.0) / 9.0) + 273.15;
   }
   
   else
+  //anything else and a kelvin temperature will be assumed
   {
     kelvin_ = temp;
   }
 }
 
 void Temperature::SetTempFromKelvin(double kelvin)
+//sets the internal temperature if already kelvin
 {
   kelvin_ = kelvin;
 }
 
 void Temperature::SetTempFromCelsius(double celsius)
+//converts temperature from celsius to kelvin and then sets it
 {
   kelvin_ = celsius + 273.15;
 }
 
 void Temperature::SetTempFromFahrenheit(double fahrenheit)
+//converts temperature from fahrenheit to kelvin and then sets it
 {
   kelvin_ = (5.0 * (fahrenheit - 32.0) / 9.0) + 273.15;
 }
 
 double Temperature::GetTempAsKelvin() const
+//returns the set temperature as kelvin
 {
   return kelvin_;
 }
 
 double Temperature::GetTempAsCelsius() const
+//returns the set temperature as celsius
 {
   double celsius;
   celsius = kelvin_ - 273.15;
@@ -63,6 +74,7 @@ double Temperature::GetTempAsCelsius() const
 }
 
 double Temperature::GetTempAsFahrenheit() const
+//returns the set temperatures as fahrenheit
 {
   double fahrenheit;
   fahrenheit = ((kelvin_ - 273.15) * 9.0 / 5.0) + 32.0;
@@ -70,6 +82,7 @@ double Temperature::GetTempAsFahrenheit() const
 }
 
 string Temperature::ToString(char unit) const
+//converts and returns the saved temperature as a string with the temperature type
 {
   stringstream ss;
   ss.str();
@@ -80,16 +93,19 @@ string Temperature::ToString(char unit) const
   ss.precision(2);
   
   if (unit == 'K' || unit == 'k')
+  //If its kelvin you want kelvin you'll get
   {
     ss << GetTempAsKelvin() << " " << "Kelvin";
   }
   
   else if (unit == 'C' || unit == 'c')
+  //If its celsius you want celsius you'll get
   {
     ss << GetTempAsCelsius() << " " << "Celsius";
   }
   
   else if (unit == 'F' || unit == 'f')
+  //If its fahrenheit you want fahrenheit you'll get
   {
     ss << GetTempAsFahrenheit() << " " << "Fahrenheit";
   }

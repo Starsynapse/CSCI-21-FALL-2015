b4a7177fb861c0f4dac2191cda9aceb6a74de6ac
starsynapse <starsynapse@gmail.com>
2016-03-04 06:52:44 +0000

    gonna make the makefile

--- topic2/lab11/money.cpp
+++ topic2/lab11/money.cpp
@@ -49,49 +49,39 @@ const Money operator +(const Money& amount1, const Money& amount2) {
 }
 
 // REST OF CLASS FUNCTION DEFINITIONS GO HERE
-Money::Money(int dollars, int cents)
-{
-  dollars_ = dollars;
-  cents_ = cents;
-}
-
-int Money::dollars() const
-{
-  return dollars_;
-}
-
-int Money::cents() const
-{
-  return cents_;
-}
-
-void Money::set_dollars(int dollars)
-{
-  dollars_ = dollars;
-}
-
-void set_cents(int cents)
-{
-  cents_ = cents;
-}
-
-const Money operator +(const Money& amount1, const Money& amount2)
-{
-  return (ammount1.Money + ammount2.Money)
-}
-
 const Money operator -(const Money& amount1, const Money& amount2)
 {
-  return (ammount1.Money - ammount2.Money)
+  // Get all the cents of object 1
+  int all_cents1 = amount1.cents_ + amount1.dollars_ * 100;
+  // Get all the cents of object 2
+  int all_cents2 = amount2.cents_ + amount2.dollars_ * 100;
+  // Subtract cents of object 2 from object 1
+  int dif_all_cents = all_cents1 - all_cents2;
+  // Handle the fact that money can be negative
+  int abs_all_cents = abs(dif_all_cents);
+  int final_dollars = abs_all_cents / 100;
+  int final_cents = abs_all_cents % 100;
+  // If the result of the operation was negative, negative final dollars and cents
+  if (dif_all_cents < 0)
+  {
+    final_dollars = -final_dollars;
+    final_cents = -final_cents;
+  }
+  return Money(final_dollars, final_cents);
 }
 
 bool operator ==(const Money &amount1, const Money &amount2)
 {
-  if (ammount1.Money == ammount2.Money)
+  // Get all the cents of object 1
+  int all_cents1 = amount1.cents_ + amount1.dollars_ * 100;
+  // Get all the cents of object 2
+  int all_cents2 = amount2.cents_ + amount2.dollars_ * 100;
+  // Compares cents of both objects
+  if (all_cents1 == all_cents2)
   {
     return true;
   }
-  
+  // Returns false if not equal
   else
   {
     return false;
@@ -100,10 +90,23 @@ bool operator ==(const Money &amount1, const Money &amount2)
 
 const Money operator -(const Money &amount)
 {
-  return (-ammount.Money)
+  // Get all the cents of the object
+  int all_cents = amount.cents_ + amount.dollars_ * 100;
+  // Converts cents to negative cents
+  int final_dollars = -(all_cents / 100);
+  int final_cents = -(all_cents % 100);
+  return Money(final_dollars, final_cents);
 }
 
 ostream& operator <<(ostream &out, const Money &amount)
 {
+  // Get all the cents of the object
+  int all_cents = amount.cents_ + amount.dollars_ * 100;
+  // Handle the fact that money can be negative
+  int abs_all_cents = abs(sum_all_cents);
+  int final_dollars = abs_all_cents / 100;
+  int final_cents = abs_all_cents % 100;
   
+  ostream << final_dollars << final_cents;
+  return out(ostream);
 }

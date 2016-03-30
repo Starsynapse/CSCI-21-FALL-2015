8256054187460b600de90b5f7148afacfe694f9a
starsynapse <starsynapse@gmail.com>
2016-03-30 23:11:37 +0000

    working on lab16, assignment3 still pending

---
+++ topic3/lab16/sl_list.cpp
@@ -0,0 +1,27 @@
+/*
+ * Name        : sl_list.cpp
+ * Author      : Eduardo
+ * Description : 
+ */
+
+#include "sl_list.h"
+
+SLList::SLList()
+{
+    head_ = NULL;
+    size_ = 0;
+}
+
+SLList::~SLList()
+{
+    Clear();
+}
+
+void InsertHead(int new_head)
+{
+    SLNode *temp = new SLNode(new_head);
+    //creates temp node and makes it equal to the new_head information
+    temp -> set_next_node(head_);
+    //temp points to the next node "head_"
+    head_ = temp;
+}

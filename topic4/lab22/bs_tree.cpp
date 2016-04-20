98e976304ce8c13046906c48e96b4e4e7ab4268f
starsynapse <starsynapse@gmail.com>
2016-04-20 23:51:20 +0000

    lab22 getting done

---
+++ topic4/lab22/bs_tree.cpp
@@ -0,0 +1,51 @@
+/*
+ * Name        : bs_node.cpp
+ * Author      : Eduardo
+ * Description : 
+ */
+
+bool BSTree::Insert(int, BSTNode*&)
+{
+    
+}
+
+void BSTree::Clear(BSTNode*&)
+{
+    
+}
+
+string BSTree::InOrder(BSTNode*)
+{
+    
+}
+
+BSTree::BSTree()
+{
+    root_ = NULL;
+    size_ = 0;
+}
+
+BSTree::~BSTree()
+{
+    Clear();
+}
+
+bool BSTree::Insert(int)
+{
+    Insert(int, root)
+}
+
+void BSTree::Clear()
+{
+    Clear(root);
+}
+
+unsigned int BSTree::size() const
+{
+    return size_;
+}
+
+string BSTree::InOrder()
+{
+    InOrder(root);
+}

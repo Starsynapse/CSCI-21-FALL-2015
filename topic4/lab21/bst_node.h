98988fb6140ff9c4df2640df6495cfbb7697922e
starsynapse <starsynapse@gmail.com>
2016-04-20 04:18:11 +0000

    lab21 done

--- topic4/lab21/bst_node.h
+++ topic4/lab21/bst_node.h
@@ -1,5 +1,5 @@
 /*
- * Name        : bts_node.h
+ * Name        : BST_node.h
  * Author      : Eduardo
  * Description : 
  */
@@ -7,35 +7,55 @@
 #ifndef LAB_H
 #define LAB_H
 
+#include <iostream>
+using namespace std;
+
 class BSTNode
 {
   private:
-    BTSNode* left_child_;
+    BSTNode* left_child_;
     //points to a left child
     
-    BTSNode* right_child_;
+    BSTNode* right_child_;
     //points to a right child
     
     int contents_;
     //used to store the data contents of a BSTNode
     
   public:
-    BTSNode();
+    BSTNode();
     //sets left_child to NULL
     //sets right_child to NULL
     //sets contents_ to 0
     
-    BTSNode(int contents);
+    BSTNode(int contents);
     //sets left_child_ to NULL
     //sets left_child_ to NULL
     //sets contents_ to the value of the parameter
     
-    ~BTSNode();
+    ~BSTNode();
     //sets left_child_ to NULL
     //sets right_child_ to NULL
 
-    void set_contents(int);
-    //
+    void set_contents(int contents);
+    //mutator for contents
+    
+    int contents() const;
+    int& contents();
+    //accessors for contents_
+    
+    void set_left_child(BSTNode* left_child);
+    //mutator for left_child_
+    void set_right_child(BSTNode* right_child);
+    //mutator for right_child_
+    
+    BSTNode* left_child() const;
+    BSTNode*& left_child();
+    //accessors for left_child_
+    
+    BSTNode* right_child() const;
+    BSTNode*& right_child();
+    //accessors for right_child_
 };
 
 #endif
